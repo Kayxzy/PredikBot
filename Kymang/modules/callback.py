5dde4410503e582ec7ec04f1701630cbd46c2d88
kay-ex <106707683+Kayxzy@users.noreply.github.com>
2025-01-31 21:00:21 +0700

    Update callback.py

--- Kymang/modules/callback.py
+++ Kymang/modules/callback.py
@@ -152,6 +152,7 @@ async def _(c, callback_query: CallbackQuery):
         return
     user_id = callback_query.from_user.id
     kymang = await cek_seller()
+    if user_id not in MEMBER and user_id not in kymang:ang = await cek_seller()
     if user_id not in MEMBER and user_id not in kymang:
         await callback_query.message.edit(
             "**🤖 Buat Fsub Bot**\n\n**Untuk mengakses fitur Premium ini, Anda perlu melakukan pembelian.**\n**Beli sekarang untuk bisa membuat Bot Fsub Premium!**",
@@ -169,6 +170,12 @@ async def _(c, callback_query: CallbackQuery):
         ),
     )
         return
+
+
+# Menangani callback untuk mendapatkan prediksi lain
+@app.on_callback_query(filters.regex("get_another_prediction"))
+async def get_another_prediction(_, callback_query):
+    user_id = callback_query.from_user.id  # Mendapatkan ID pengguna
     current_time = datetime.now()
 
     # Cek apakah pengguna sudah menggunakan perintah dalam 1 menit terakhir
@@ -190,9 +197,7 @@ async def _(c, callback_query: CallbackQuery):
 
     # Mengedit pesan dengan prediksi baru dan waktu yang diprediksi
     await callback_query.message.edit_text(f"{bar}\n\n**Waktu prediksi (WIB):** {formatted_time}", reply_markup=callback_query.message.reply_markup)
-    
-    
-    
+
 
 @bot.on_callback_query(filters.regex("support"))
 async def _(c, callback_query: CallbackQuery):
