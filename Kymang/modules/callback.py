b63aeda3ef705f776f34a3df1519ca44985f796f
kay-ex <106707683+Kayxzy@users.noreply.github.com>
2025-01-31 22:27:06 +0700

    Update callback.py

--- Kymang/modules/callback.py
+++ Kymang/modules/callback.py
@@ -154,7 +154,16 @@ async def buat_bot(c, callback_query: CallbackQuery):
     user_id = callback_query.from_user.id
     if user_id not in MEMBER:
         await callback_query.answer(
-            "**Untuk mengakses fitur Premium ini, Anda perlu melakukan pembelian.**\n**Beli sekarang untuk menggunakan Predictor**")
+            "**Untuk mengakses fitur Premium ini, Anda perlu melakukan pembelian.**\n**Beli sekarang untuk menggunakan Predictor**",
+        )
+        await callback_query.message.edit_reply_markup(
+            InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(text="👨‍💻 Admin", user_id=1399943740)],
+                    [InlineKeyboardButton("Back", callback_data="back_start")],
+                ]
+            )
+        )
         return
         
     current_time = datetime.now()
